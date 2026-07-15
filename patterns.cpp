639372983bf8932cd218594f1fb72688ed569d79
Saim Kazmi <saimkazmi017@gmail.com>
2026-07-15 11:52:03 +0530

    uploading 6 patterns

---
+++ patterns.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+void pattern1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern2(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<" * ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+void pattern3(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j;
+        }
+        
+        cout<<endl;
+    }
+}
+void pattern4(int n){
+    for(int i = 1; i <= n; i ++){
+        for(int j = 1; j <= i; j++){
+            cout<<i;
+        }
+        cout << endl;
+    }
+}
+void pattern5(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= n - i; j++)
+        {
+            cout<<"*";
+        }
+        cout<< endl;
+    }
+    
+}
+void pattern6(int n){
+    for(int i = 1; i <= n; i ++ ){
+        for (int j = 1; j <= n-i+1; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int user_input;
+    cin >> user_input;
+    pattern6(user_input);
+
+    return 0;
+}
