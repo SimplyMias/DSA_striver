07173103e9da40e8cac05ed78a46805d3866b9fd
Saim Kazmi <saimkazmi017@gmail.com>
2026-07-19 14:50:16 +0530

    11 Patterns completed

--- patterns.cpp
+++ patterns.cpp
@@ -11,62 +11,148 @@ void pattern1(int n)
         cout << endl;
     }
 }
-void pattern2(int n){
+void pattern2(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
         {
-            cout<<" * ";
+            cout << " * ";
         }
-        
-        cout<<endl;
+
+        cout << endl;
     }
-    
 }
-void pattern3(int n){
+void pattern3(int n)
+{
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout<<j;
+            cout << j;
         }
-        
-        cout<<endl;
+
+        cout << endl;
     }
 }
-void pattern4(int n){
-    for(int i = 1; i <= n; i ++){
-        for(int j = 1; j <= i; j++){
-            cout<<i;
+void pattern4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
         }
         cout << endl;
     }
 }
-void pattern5(int n){
+void pattern5(int n)
+{
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j <= n - i; j++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        cout<< endl;
+        cout << endl;
     }
-    
 }
-void pattern6(int n){
-    for(int i = 1; i <= n; i ++ ){
-        for (int j = 1; j <= n-i+1; j++)
+void pattern6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
         {
-            cout<<j;
+            cout << j;
         }
-        cout<<endl;
+        cout << endl;
+    }
+}
+void pattern7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < (2 * n) - (2 * i) - 1; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void pattern9(int n)
+{
+
+    pattern7(n);
+    pattern8(n);
+}
+void pattern10(int n)
+{
+    for (int i = 1; i <= (2 * n - 1); i++)
+    {
+        int stars = i;
+        if (i > n)
+        {
+            stars = 2 * n - i;
+        }
+
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern11(int n)
+{
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
+
+            cout << start << " ";
+            start = 1 - start;
+        }
+        cout << endl;
     }
 }
 int main()
 {
     int user_input;
     cin >> user_input;
-    pattern6(user_input);
+    pattern11(user_input);
 
     return 0;
 }
